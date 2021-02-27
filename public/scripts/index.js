05f22a19b7c8215f4e49de93f3b1fb4dba534a49
Vivek Chandra <vivek.chandra@t-systems.com>
2021-02-27 18:45:28 +0530

    Made Photography page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,37 +6,48 @@ var app = new Vue({
 });
 
 $(document).ready(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-    $('#logo').fadeIn(1000);
-    $('#btnMenuCollapse').click(function () {
-        $('.nav li a').css('background', 'rgba(231, 76, 60, 1.0)');
-    });
-    
+  $("#imageFullScreen").hide();
+  $('[data-toggle="tooltip"]').tooltip();
+  $("#logo").fadeIn(1000);
+  $("#btnMenuCollapse").click(function () {
+    $(".nav li a").css("background", "rgba(231, 76, 60, 1.0)");
+  });
+  $("#imageFullScreen .close-icon").click(function  ()  {
+    $("#imageFullScreen").hide();
+   });
 });
 
 function topMenuClicked(menuItemName) {
-    switch (menuItemName) {
-        case 'HOME':
-            $('#content').load('../index.html');
-            break;
-        case 'PROJECTS':
-            $('#content').load('../projects.html');
-            break;
-        case 'APPS':
-            $('#content').load('../apps.html');
-            break;
-        case 'PHOTOGRAPHY':
-            $('#content').load('../photography.html');
-            break;
-        case 'LITERARY WORKS':
-            $('#content').load('../literary-works.html');
-            break;
-        case 'IDEAS':
-            $('#content').load('../ideas.html');
-            break;
-        default:
-            $('#content').html('NO CONTENT FOUND!');
-            break;
-    }
-    $('.navbar-collapse.navbar-right.collapse[aria-expanded="true"]').removeClass('in');
+  switch (menuItemName) {
+    case "HOME":
+      $("#content").load("../index.html");
+      break;
+    case "PROJECTS":
+      $("#content").load("../projects.html");
+      break;
+    case "APPS":
+      $("#content").load("../apps.html");
+      break;
+    case "PHOTOGRAPHY":
+      $("#content").load("../photography.html");
+      break;
+    case "LITERARY WORKS":
+      $("#content").load("../literary-works.html");
+      break;
+    case "IDEAS":
+      $("#content").load("../ideas.html");
+      break;
+    default:
+      $("#content").html("NO CONTENT FOUND!");
+      break;
+  }
+  $('.navbar-collapse.navbar-right.collapse[aria-expanded="true"]').removeClass(
+    "in"
+  );
+}
+
+function showImageFullScreen(imagePath) {
+  console.log(imagePath);
+  $("#imageFullScreen .fullScreen").attr("src", imagePath);
+  $("#imageFullScreen").fadeIn();
 }
